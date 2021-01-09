6563c7031f7930574426df7517170a562c7965be
linabbc <64610798+linabbc@users.noreply.github.com>
2021-01-09 22:40:18 +0100

    Add files via upload

--- projet/app.py
+++ projet/app.py
@@ -3,25 +3,41 @@ import numpy as np
 import pickle as p
 import json
 from joblib import load
-import sqlite3
+
+
+
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/site/')
 def index():
-    conn = get_db_connection()
-    online_shoppers_intention = conn.execute('SELECT * FROM online_shoppers_intention').fetchall()
-    conn.close()
-    return render_template('index.html', posts=online_shoppers_intention)
+    return render_template('index.html')
+
+@app.route('/site/',methods=['POST'])
+def form_online_shoppers():
+    Administrative = int(request.form['Administrative'])
+    Administrative_Duration = float(request.form['Administrative_Duration'])
+    Informational = int(request.form['Informational'])
+    Informational_Duration = float(request.form['Informational_Duration'])
+    ProductRelated = int(request.form['ProductRelated'])
+    ProductRelated_Duration = float(request.form['ProductRelated_Duration'])
+    BounceRates = float(request.form['BounceRates'])
+    ExitRates = float(request.form['ExitRates'])
+    PageValues = float(request.form['PageValues'])
+    SpecialDay = float(request.form['SpecialDay'])
+    Month = int(request.form['Month'])
+    OperatingSystems = int(request.form['OperatingSystems'])
+    Browser = int(request.form['Browser'])
+    Region = int(request.form['Region'])
+    TrafficType = int(request.form['TrafficType'])
+    VisitorType = int(request.form['VisitorType'])
+    Weekend = int(request.form['Weekend'])
+    data = [[Administrative,Administrative_Duration,Informational,Informational_Duration,ProductRelated,ProductRelated_Duration,BounceRates,ExitRates,PageValues,SpecialDay,Month,OperatingSystems,Browser,Region,TrafficType,VisitorType,Weekend]]
+    prediction = np.array2string(model.predict(data))
+    return render_template("result.html",resultat= prediction)
 
 
-
-#def get_db_connection():
- #   conn = sqlite3.connect('database.db')
-  #  conn.row_factory = sqlite3.Row
-   # return conn
-
-@app.route('/api/', methods=['POST'])
-def makecalc():
+@app.route('/local/', methods=['POST'])
+def local():
     data = request.get_json()
     prediction = np.array2string(model.predict(data))
     return jsonify(prediction)
