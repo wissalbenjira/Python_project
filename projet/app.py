9d8f8b4bb024625176c49128111e65563543d8c0
linabbc <64610798+linabbc@users.noreply.github.com>
2021-01-09 17:23:19 +0100

    Add files via upload

--- projet/app.py
+++ projet/app.py
@@ -1,10 +1,24 @@
-from flask import Flask, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 import numpy as np
 import pickle as p
 import json
 from joblib import load
+import sqlite3
 
 app = Flask(__name__)
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    online_shoppers_intention = conn.execute('SELECT * FROM online_shoppers_intention').fetchall()
+    conn.close()
+    return render_template('index.html', posts=online_shoppers_intention)
+
+
+
+#def get_db_connection():
+ #   conn = sqlite3.connect('database.db')
+  #  conn.row_factory = sqlite3.Row
+   # return conn
 
 @app.route('/api/', methods=['POST'])
 def makecalc():
