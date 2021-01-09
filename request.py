9b023fc5d6689466fb06a1d15b1f9e4546c992e6
linabbc <64610798+linabbc@users.noreply.github.com>
2021-01-09 22:40:52 +0100

    Add files via upload

--- request.py
+++ request.py
@@ -1,9 +1,12 @@
 import requests
 import json
 
-url = 'http://127.0.0.1:8080/api/'
+url = 'http://127.0.0.1:5000/site/'
 
-data = [4,64.6,0,0,32,1135.444444,0.002857143,0.00952381,0,0,'Feb',2,2,1,3,'Returning_Visitor',False]
+
+
+
+data = [[4,64.6,0,0,32,1135.444444,0.002857143,0.00952381,0,0,2,2,2,1,3,2,0]]
 j_data = json.dumps(data)
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 r = requests.post(url, data=j_data, headers=headers)
