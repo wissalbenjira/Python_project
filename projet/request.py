b23b4276ebce02f9b8f11a0efcd99ce2c4e7628e
linabbc <64610798+linabbc@users.noreply.github.com>
2021-01-10 00:09:41 +0100

    Add files via upload

--- projet/request.py
+++ projet/request.py
@@ -1,7 +1,10 @@
 import requests
 import json
 
-url = 'http://127.0.0.1:5000/api/'
+url = 'http://127.0.0.1:5000/local/'
+
+
+
 
 data = [[4,64.6,0,0,32,1135.444444,0.002857143,0.00952381,0,0,2,2,2,1,3,2,0]]
 j_data = json.dumps(data)
